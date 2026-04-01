6ca89a8ed66b9cd6809d945ba46a92124983c911
tsubasa namatame <nyshk97@gmail.com>
2026-04-01 16:17:59 +0900

    feat: macOSアプリをメニューバー常駐のポップオーバー形式に変更

--- apps/macos/Sources/TodoApp.swift
+++ apps/macos/Sources/TodoApp.swift
@@ -2,11 +2,45 @@ import SwiftUI
 
 @main
 struct TodoApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        Settings {
+            EmptyView()
         }
-        .windowStyle(.hiddenTitleBar)
-        .defaultSize(width: 380, height: 600)
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate, NSPopoverDelegate {
+    private var statusItem: NSStatusItem!
+    private var popover: NSPopover!
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Dock アイコンを非表示
+        NSApp.setActivationPolicy(.accessory)
+
+        // ポップオーバー
+        popover = NSPopover()
+        popover.contentSize = NSSize(width: 360, height: 520)
+        popover.behavior = .transient
+        popover.delegate = self
+        popover.contentViewController = NSHostingController(rootView: ContentView())
+
+        // メニューバーアイコン
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+        if let button = statusItem.button {
+            button.image = NSImage(systemSymbolName: "checklist", accessibilityDescription: "Todo")
+            button.action = #selector(togglePopover)
+        }
+    }
+
+    @objc func togglePopover() {
+        guard let button = statusItem.button else { return }
+        if popover.isShown {
+            popover.performClose(nil)
+        } else {
+            popover.show(relativeTo: button.bounds, of: button, preferredEdge: .minY)
+            NSApp.activate(ignoringOtherApps: true)
+        }
     }
 }
